b7e7be416f13c4bb4f070f557498b4829a246471
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2022-10-27 16:41:03 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,119 @@
+import pandas as pd  # pip install pandas openpyxl
+import plotly.express as px  # pip install plotly-express
+import streamlit as st  # pip install streamlit
+import streamlit_authenticator as stauth  # pip install streamlit-authenticator
+import numpy as np
+import database as db
+import time
+
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(
+    page_title="Real-Time Data Science Dashboard",
+    page_icon="✅",
+    layout="wide",
+)
+
+
+# --- DEMO PURPOSE ONLY --- #
+placeholder = st.empty()
+placeholder.info("CREDENTIALS | username:Soumen Sarker; password:abc123")
+# ------------------------- #
+
+# --- USER AUTHENTICATION ---
+users = db.fetch_all_users()
+
+usernames = [user["key"] for user in users]
+names = [user["name"] for user in users]
+hashed_passwords = [user["password"] for user in users]
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "sales_dashboard", "abcdef", cookie_expiry_days=30)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+
+if authentication_status:
+    # read csv from a github repo
+    dataset_url = "https://raw.githubusercontent.com/Lexie88rus/bank-marketing-analysis/master/bank.csv"
+
+    # read csv from a URL
+    @st.experimental_memo
+    def get_data() -> pd.DataFrame:
+        return pd.read_csv(dataset_url)
+
+    df = get_data()
+
+    # dashboard title
+    st.title("Real-Time / Live Data Science Dashboard")
+
+    # top-level filters
+    job_filter = st.selectbox("Select the Job", pd.unique(df["job"]))
+
+    # creating a single-element container
+    placeholder = st.empty()
+
+    # dataframe filter
+    df = df[df["job"] == job_filter]
+
+    # near real-time / live feed simulation
+    for seconds in range(200):
+
+        df["age_new"] = df["age"] * np.random.choice(range(1, 5))
+        df["balance_new"] = df["balance"] * np.random.choice(range(1, 5))
+
+        # creating KPIs
+        avg_age = np.mean(df["age_new"])
+
+        count_married = int(
+            df[(df["marital"] == "married")]["marital"].count()
+            + np.random.choice(range(1, 30))
+        )
+
+        balance = np.mean(df["balance_new"])
+
+        with placeholder.container():
+
+            # create three columns
+            kpi1, kpi2, kpi3 = st.columns(3)
+
+            # fill in those three columns with respective metrics or KPIs
+            kpi1.metric(
+                label="Age ⏳",
+                value=round(avg_age),
+                delta=round(avg_age) - 10,
+            )
+            
+            kpi2.metric(
+                label="Married Count 💍",
+                value=int(count_married),
+                delta=-10 + count_married,
+            )
+            
+            kpi3.metric(
+                label="A/C Balance ＄",
+                value=f"$ {round(balance,2)} ",
+                delta=-round(balance / count_married) * 100,
+            )
+
+            # create two columns for charts
+            fig_col1, fig_col2 = st.columns(2)
+            with fig_col1:
+                st.markdown("### First Chart")
+                fig = px.density_heatmap(
+                    data_frame=df, y="age_new", x="marital"
+                )
+                st.write(fig)
+                
+            with fig_col2:
+                st.markdown("### Second Chart")
+                fig2 = px.histogram(data_frame=df, x="age_new")
+                st.write(fig2)
+
+            st.markdown("### Detailed Data View")
+            st.dataframe(df)
+            time.sleep(1)
